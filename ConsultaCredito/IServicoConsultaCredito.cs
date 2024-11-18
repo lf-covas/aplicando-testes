5070c8823a31d825d07cc90c635456b58b814759
lfcovas <110571286+lf-covas@users.noreply.github.com>
2024-11-18 19:12:52 +0000

    feat: Adicionando feature de Consulta de Creedito

---
+++ ConsultaCredito/IServicoConsultaCredito.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace ConsultaCredito
+{
+    public interface IServicoConsultaCredito
+    {
+        IList<Pendencia> ConsultarPendenciasPorCPF(string cpf);
+    }
+}
